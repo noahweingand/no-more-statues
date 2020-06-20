3b9b9f589092e2048d97779ddcc69952ad980bd6
Noah Weingand <noahweingand@gmail.com>
2020-06-20 16:31:33 -0400

    added Google Maps secret

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const api_key = process.env.API_KEY;
 
 const app = express();
 app.use(bodyParser.json());
