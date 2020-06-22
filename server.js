93968a3b727bff65608635005ea56d709811b20a
Noah Weingand <noahweingand@gmail.com>
2020-06-22 17:40:53 -0400

    call dynamodb w/ promise

--- server.js
+++ server.js
@@ -13,32 +13,20 @@ var params = { TableName: 'no-more-statues' };
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-docClient.scan(params, onScan);
-var scanRes = [];
+var scanPromise = docClient.scan(params).promise();
 
-function onScan(err,data) {
-    if (err) {
-        console.error("Unable to scan the table. Error JSON: ", JSON.stringify(err, null, 2));
-    }
-    else {
-        console.log("Scan successful.");
-        data.Items.forEach(function(itemdata) {
-            scanRes.push(JSON.stringify(itemdata));
-        });
-
-        //continue scanning if more items
-        // if (typeof data.LastEvaluatedKey != "undefined") {
-        //     console.log("Scanning again...");
-        //     params.ExclusiveStartKey = data.LastEvaluatedKey;
-        //     docClient.scan(params, onScan);
-        // }
-    }
-}
-
-app.get('/', function (req, res) {
-    res.send(scanRes);
+app.get('/getAllStatues', function (req, res) {
+    scanPromise.then(
+        function(data) {
+            console.log("scan successful");
+            res.send(data)
+        },
+        function(error) {
+            console.log("error on /getAllStatues")
+            res.send(error);
+        }
+    )
 })
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
