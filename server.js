922f0cb25aaeed43bfbf363bc7987e2841852dc8
Noah Weingand <noahweingand@gmail.com>
2020-06-22 19:51:59 -0400

    added routes for querying

--- server.js
+++ server.js
@@ -13,16 +13,102 @@ var params = { TableName: 'no-more-statues' };
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var scanPromise = docClient.scan(params).promise();
-
 app.get('/getAllStatues', function (req, res) {
+    var scanPromise = docClient.scan(params).promise();
     scanPromise.then(
         function(data) {
-            console.log("scan successful");
+            console.log("/getAllStatues call successful");
             res.send(data)
         },
         function(error) {
-            console.log("error on /getAllStatues")
+            console.log("error on /getAllStatues call")
+            res.send(error);
+        }
+    )
+})
+
+app.post('/getStatuesByDate', function (req, res) {
+    var dateParams = {
+        TableName: "no-more-statues",
+        FilterExpression: "begins_with (removalDate, :date)",
+        ExpressionAttributeValues: {
+            ":date": req.body.date
+        }
+    }
+    let datePromise = docClient.scan(dateParams).promise();
+    datePromise.then(
+        function(data) {
+            console.log("/getStatuesByDate call successful");
+            res.send(data)
+        },
+        function(error) {
+            console.log("error on /getStatuesByDate call")
+            res.send(error);
+        }
+    )
+})
+
+app.post('/getStatuesByName', function (req, res) { 
+    var nameParams = {
+        TableName: "no-more-statues",
+        FilterExpression: "begins_with (#name, :name)", // case sensitive, probably need to change
+        ExpressionAttributeNames: {
+            "#name": "name" // name is a reserved word with dynamodb, so work around
+        },
+        ExpressionAttributeValues: {
+            ":name": req.body.name
+        }
+    }
+    let namePromise = docClient.scan(nameParams).promise();
+    namePromise.then(
+        function(data) {
+            console.log("/getStatuesByName call successful");
+            res.send(data)
+        },
+        function(error) {
+            console.log("error on /getStatuesByName call")
+            res.send(error);
+        }
+    )
+})
+
+app.get('/getRemovedStatues', function (req, res) { 
+    var isRemovedParams = {
+        TableName: "no-more-statues",
+        FilterExpression: "removed = :status",
+        ExpressionAttributeValues: {
+            ":status": true
+        }
+    }
+    let removedPromise = docClient.scan(isRemovedParams).promise();
+    removedPromise.then(
+        function(data) {
+            console.log("/getRemovedStatues call successful");
+            res.send(data)
+        },
+        function(error) {
+            console.log("error on /getRemovedStatues call")
+            res.send(error);
+        }
+    )
+})
+
+app.get('/getStandingStatues', function (req, res) { 
+    var isRemovedParams = {
+        TableName: "no-more-statues",
+        FilterExpression: "removed = :status",
+        ExpressionAttributeValues: {
+            ":status": false
+        }
+    }
+    let standingPromise = docClient.scan(isRemovedParams).promise();
+    standingPromise.then(
+        function(data) {
+            console.log("/getStandingStatues call successful");
+            res.send(data)
+        },
+        function(error) {
+            console.log("error on /getStandingStatues call")
             res.send(error);
         }
     )
