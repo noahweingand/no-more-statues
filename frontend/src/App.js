a0e42ea499bb0836d2221de37d8769d8b1b45e31
Noah Weingand <noahweingand@gmail.com>
2020-06-23 20:55:30 -0400

    frontend initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Main from './components/Main.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <Main/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
