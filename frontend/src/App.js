8eb2295f1d4961d747a2894810e489577efe0a90
Noah Weingand <noahweingand@gmail.com>
2020-06-24 20:02:51 -0400

    working leaflet map

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import Main from './components/Main.js';
+//import Main from './components/Main.js';
+import 'leaflet/dist/leaflet.css'
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <Main/>
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+      lat: 30,
+      lng: -20,
+      zoom: 2,
+  }
+  render() { 
+      const position = [this.state.lat, this.state.lng];
+      return ( 
+          <div>
+              <h1>Hello No-More-Statues!</h1>
+              <Map style={{ width: '100%', height: '600px' }} center={position} zoom={this.state.zoom}>
+                  <TileLayer
+                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  />
+                  <Marker position={position}>
+                  <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                  </Marker>
+              </Map> 
+          </div>
+      );
+  }
 }
 
 export default App;
