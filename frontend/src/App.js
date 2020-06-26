413ff6bd33335dc6a816570d7ab33850303dbc78
Noah Weingand <noahweingand@gmail.com>
2020-06-26 18:18:17 -0400

    added About modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'leaflet/dist/leaflet.css'
@@ -6,7 +6,7 @@ import L from 'leaflet';
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import axios from 'axios';
 import MapIcon from './img/mapIcon.png';
-import {Navbar, Nav, NavDropdown, Button, FormControl, Form } from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown, Button, FormControl, Form, Modal } from 'react-bootstrap';
 
 class App extends Component {
     constructor(props) {
@@ -22,8 +22,12 @@ class App extends Component {
                 iconSize:     [50, 50], // size of the icon
                 iconAnchor:   [0, 0], // 10,15 point of the icon which will correspond to marker's location
                 popupAnchor:  [25, 0] //-3,-10
-            })
+            }),
+            showHide: false
         }
+    }
+    handleModalShowHide() {
+        this.setState({ showHide: !this.state.showHide })
     }
     componentDidMount() {
         axios.get('http://localhost:8080/api/statues/all')
@@ -37,12 +41,12 @@ class App extends Component {
         return ( 
             <div>
                 <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">no more statues</Navbar.Brand>
+                    <Navbar.Brand href="/">no more statues</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        <Nav.Link href="#home">About</Nav.Link>
-                        <Nav.Link href="#link">Github</Nav.Link>
+                        <Nav.Link onClick={() => this.handleModalShowHide()}>About</Nav.Link>
+                        <Nav.Link href="https://github.com/noahweingand/no-more-statues" target="_blank">Github</Nav.Link>
                         </Nav>
                         <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
@@ -50,13 +54,24 @@ class App extends Component {
                             <NavDropdown.Item href="#action/3.1">Statue</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Date Removed</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.3">Standing</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.4">Removed</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Removed</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Standing</NavDropdown.Item>
                         </NavDropdown>
-                        <Button variant="">Search</Button>
+                        <Button variant="primary">Search</Button>
                         </Form>
                     </Navbar.Collapse>
                 </Navbar>
+                <Modal show={this.state.showHide}>
+                    <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
+                    <Modal.Title>About</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Hello, this is blah blah BLM</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={() => this.handleModalShowHide()}>
+                        Close
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
                 <Map style={{ width: '100%', height: '600px' }} center={position} zoom={this.state.startZoom}>
                     <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
